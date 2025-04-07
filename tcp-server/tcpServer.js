cd95c66fe168532a790f024d890eae819781aef6
Dimitar Nizamov <dimitur2204@gmail.com>
2025-04-07 14:28:40 +0200

    add tcp js server

---
+++ tcp-server/tcpServer.js
@@ -0,0 +1,37 @@
+const net = require("net");
+
+const PORT = 1337;
+const HOST = "0.0.0.0";
+
+const server = net.createServer(
+    {
+        keepAlive: true,
+    },
+  (socket) => {
+    console.log(
+      "Client connected:",
+      socket.remoteAddress + ":" + socket.remotePort,
+    );
+
+    socket.on("data", (data) => {
+      console.log("Data received from client:", data.toString());
+      socket.write("Server received: " + data.toString());
+    });
+
+    socket.on("end", () => {
+      console.log("Client disconnected");
+    });
+
+    socket.on("error", (err) => {
+      console.error("Socket error:", err);
+    });
+  },
+);
+
+server.listen(PORT, HOST, () => {
+  console.log("Server listening on " + HOST + ":" + PORT);
+});
+
+server.on("error", (err) => {
+  console.error("Server error:", err);
+});
