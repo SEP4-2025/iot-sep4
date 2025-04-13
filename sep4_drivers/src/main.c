abc17894fa44579597f1cf376c113cfbaeda6b94
Dimitar Nizamov <dimitur2204@gmail.com>
2025-04-13 13:57:11 +0200

    remove cache and vscode

--- sep4_drivers/src/main.c
+++ sep4_drivers/src/main.c
@@ -36,40 +36,6 @@ void console_rx(uint8_t _rx)
 
 static void my_event_cb(const struct event *evt, void *data)
 {
-<<<<<<< HEAD
-    wifi_init();
-    light_init();
-    sei();
-    uart_init(USART_0, 9600, console_rx);
-    uart_send_string_blocking(USART_0, "Hello\n");
-    wifi_command_create_TCP_connection("10.121.138.177", 1883, NULL, NULL);
-    WIFI_ERROR_MESSAGE_t wifi_res = wifi_command_join_AP("Dimitar's Pixel 7 Pro", "1234qwert");
-    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
-    int rc = 0;
-    char buf[200];
-    int buflen = sizeof(buf);
-    MQTTString topicString = MQTTString_initializer;
-    char *payload = "I'm alive!";
-    int payloadlen = strlen(payload);
-    int len = 0;
-
-    data.clientID.cstring = "mbed test client - Ian Craggs";
-    data.keepAliveInterval = 20;
-    data.cleansession = 1;
-    data.MQTTVersion = 3;
-
-    len = MQTTSerialize_connect(buf, buflen, &data);
-
-    topicString.cstring = "mbed NXP LPC1768";
-    len += MQTTSerialize_publish(buf + len, buflen - len, 0, 0, 0, 0, topicString, payload, payloadlen);
-    len += MQTTSerialize_disconnect(buf + len, buflen - len);
-
-    wifi_command_TCP_transmit(buf, len);
-
-    char wifi_res_msg[128];
-    sprintf(wifi_res_msg, "Error: %d", wifi_res);
-    uart_send_string_blocking(USART_0, wifi_res_msg);
-=======
     /* do stuff and things with the event */
     uart_send_string_blocking(USART_0, "Hello from callback!\n");
 }
@@ -77,15 +43,11 @@ static void my_event_cb(const struct event *evt, void *data)
 void send_light_data_to_tcp() 
 {
     // Read the light
->>>>>>> main
     uint16_t light = light_read();
     char light_str[128] = "";
 
     // Send the light data to the tcp server 
     sprintf(light_str, "Light data: %d", light);
-<<<<<<< HEAD
-    while (1)
-=======
     wifi_command_TCP_transmit((uint8_t *)light_str, strlen(light_str));
     // uart_send_string_blocking(USART_0, "I am in a while loop!\n");
 }
@@ -115,7 +77,6 @@ int main()
     uart_send_string_blocking(USART_0, wifi_res_msg);
 
     if (wifi_res != WIFI_OK)
->>>>>>> main
     {
         uart_send_string_blocking(USART_0, "Error connecting to wifi!\n");
         return -1;
