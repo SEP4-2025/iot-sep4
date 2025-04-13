e5497a146d5b77046fe1af998d00bb6d399b93a9
Dimitar Nizamov <dimitur2204@gmail.com>
2025-04-13 18:58:50 +0200

    change to mqtt transmission with separate functions

--- tcp-server/tcpServer.js
+++ tcp-server/tcpServer.js
@@ -1,41 +1,8 @@
 const mqtt = require("mqtt");
 const client = mqtt.connect("mqtt://localhost:1883");
 
-const PORT = 1337;
-const HOST = "0.0.0.0";
-
-const server = net.createServer(
-  {
-    keepAlive: true,
-  },
-  (socket) => {
-    console.log(
-      "Client connected:",
-      socket.remoteAddress + ":" + socket.remotePort
-    );
-
-    socket.on("data", (data) => {
-      console.log("Data received from client:", data.toString());
-      socket.write("Server received: " + data.toString());
-    });
-
-    socket.on("end", () => {
-      console.log("Client disconnected");
-    });
-
-    socket.on("error", (err) => {
-      console.error("Socket error:", err);
-    });
-  }
-);
-
-server.listen(PORT, HOST, () => {
-  console.log("Server listening on " + HOST + ":" + PORT);
-});
-
-client.subscribe("mbed NXP LPC1768", (err) => {
+client.subscribe("light:reading", (err) => {
   client.on("message", (topic, message) => {
-    console.log("Message received in first");
     console.log(topic);
     console.log(message.toString());
   });
