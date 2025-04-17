24b73ab8ec8463f4ac5e6d4536ed9a9c53b24613
Dimitar Nizamov <dimitur2204@gmail.com>
2025-04-17 12:01:25 +0200

    change types in mqtt.h to correspond to the .c file

--- sep4_drivers/src/mqtt.h
+++ sep4_drivers/src/mqtt.h
@@ -1,5 +1,5 @@
 #include <stddef.h>
-int create_mqtt_connect_packet(unsigned char *buf, size_t buflen);
-int create_mqtt_transmit_packet(char *topic, unsigned char *payload,
+size_t create_mqtt_connect_packet(unsigned char *buf, size_t buflen);
+size_t create_mqtt_transmit_packet(char *topic, unsigned char *payload,
                                 unsigned char *buf, size_t buflen);
-int create_mqtt_disconnect_packet(unsigned char *buf, size_t buflen);
+size_t create_mqtt_disconnect_packet(unsigned char *buf, size_t buflen);
